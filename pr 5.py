4d0a035b5fd54576d3d996010b0510ba67e83db8
Nikoruso <62753545+dostal83@users.noreply.github.com>
2021-06-11 03:57:51 +0300

    Update pr 5.py

--- pr 5.py
+++ pr 5.py
@@ -1,21 +1,21 @@
+# Обработка данных временных рядов
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
+import pandas as pd # импорт библиотек
 
-def read_data(input_file):
+# Считывание исходника
+def read_data(input_file): 
     input_data = np.loadtxt(input_file, delimiter = None)
-
-    dates = pd.date_range('1950-01', periods = input_data.shape[0], freq = 'M')
-
-    output = pd.Series(input_data[:, dates], index = dates)
+    dates = pd.date_range('1950-01', periods = input_data.shape[0], freq = 'M') # Преобразование данных во временные ряды
+    output = pd.Series(input_data[:, dates], index = dates) # создание врем. рядов
     return output
 
 
-if __name__=='__main__':
-    input_file = "/Users/ao.txt"
-    timeseries = read_data(input_file)
+if __name__=='__main__': # Путь файла
+    input_file = "/QQQ/pr/wow.txt"
+    timeseries = read_data(input_file) # преобразование столбца
     
-
+    # Визуал данных
     plt.figure()
     timeseries.plot()
     plt.show()
